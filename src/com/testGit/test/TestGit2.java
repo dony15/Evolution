507676574342af0c74bd56a68a5e9b7771daeb60
DonY15 <37318228+625432639@users.noreply.github.com>
2018-08-10 22:34:04 +0800

    This code from IDEA

---
+++ src/com/testGit/test/TestGit2.java
@@ -0,0 +1,12 @@
+package com.testGit.test;
+
+/**
+ * @author DonY15
+ * @description
+ * @create 2018\8\10 0010
+ */
+public class TestGit2 {
+    public static void main(String[] args) {
+        System.out.println("This is TwoClass from GitTest");
+    }
+}
