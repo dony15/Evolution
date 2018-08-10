ae8c5fd4ebf42c5bd2084ab9985f379d297646f5
DonY15 <37318228+625432639@users.noreply.github.com>
2018-08-10 22:08:08 +0800

    Test TwoGitTest

--- src/com/testGit/test/TestGit.java
+++ src/com/testGit/test/TestGit.java
@@ -8,5 +8,6 @@ package com.testGit.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("This is Git Test===>HelloWord");
+        System.out.println("This is Git TwoTest===>ping X pong");
     }
 }
